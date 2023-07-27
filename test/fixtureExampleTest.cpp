c4cd9cb9f6339f77545633d4cc78c48876bfb18d
Divya Manivannan <104269460+Divzzz14@users.noreply.github.com>
2023-07-27 11:46:42 +0530

    Update fixtureExampleTest.cpp

--- test/fixtureExampleTest.cpp
+++ test/fixtureExampleTest.cpp
@@ -1,4 +1,6 @@
 #include "gtest/gtest.h"
+
+//code under Test
 class C{};
 
 class B
@@ -23,14 +25,60 @@ class A
   
 };
 
-TEST(TestFixtureObject, operationTest)
+
+// Unit Test Code
+
+//Fixture class that is derived from google test class
+class TestFixtureTestSuite: public testing::Test{
+//anything we write under this comes under protected
+protected:
+A* aPtr;
+B* bPtr;
+C* cPtr;
+TestFixtureTestSuite(){
+  //Initialization
+}
+~TestFixtureTestSuite(){
+   //Releasing the resources
+}
+voide SetUp()
+{
+   //Arrange
+   this->cPtr = new C();
+   this->bPtr = new B(&cPtr);
+   this->aPtr = new A(&bPtr);
+}
+
+void TearDown()
+{
+  //delete resources
+   delete cPtr;
+   delete bPtr;
+   delete aPtr;
+}
+
+};
+
+TEST_F(TestFixtureTestSuite, operationTest)
+{
+   //Act and assert
+   ASSERT_TRUE(aPtr->operation());//stack object 
+}
+
+TEST_F(TestFixtureTestSuite, operationFalseTest)
+{
+   //Act and assert
+   ASSERT_FALSE(aPtr->operation());
+}
+
+/*TEST(TestFixtureObject, operationTest)
 {
     //Arrange
    C cObj;
    B bObj(&cObj);
    A aObj(&bObj);
    //Act and assert
-   ASSERT_TRUE(aObj.operation());
+   ASSERT_TRUE(aObj.operation());//stack object 
 }
 
 TEST(TestFixtureObject, operationFalseTest)
@@ -40,6 +88,6 @@ TEST(TestFixtureObject, operationFalseTest)
    B bObj(&cObj);
    A aObj(&bObj);
    //Act and assert
-   EXPECT_FALSE(aObj.operation());
-}
+   ASSERT_FALSE(aObj.operation());
+}*/
 
